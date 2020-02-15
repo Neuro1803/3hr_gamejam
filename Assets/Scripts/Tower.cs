0df5f32e7232cc828b44cbc3826e53752d429425
Mateusz Kosior <kosior.mateusz.92@gmail.com>
2020-02-15 15:46:25 +0100

    koniec ??

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Tower : MonoBehaviour
 {
@@ -9,10 +10,13 @@ public class Tower : MonoBehaviour
     public TowerStatistics statistics;
     public GameObject target;
     public float timeSinceLastShoot;
+
+    private float timerToEnd = 20.0f;
     
     // Start is called before the first frame update
     void Start()
     {
+        damage = statistics.damage;
     }
 
     // Update is called once per frame
@@ -20,17 +24,27 @@ public class Tower : MonoBehaviour
     {
         target = FindClosestEnemy(GameObject.FindGameObjectsWithTag("Enemy"));
 
-        if (timeSinceLastShoot >= 1/statistics.fireRate && target) {
+        if (timeSinceLastShoot >= 1/statistics.fireRate) {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, (target.transform.position - transform.position), out hit, statistics.range)) {
                 Debug.DrawRay(transform.position, (target.transform.position - transform.position) * hit.distance, Color.yellow);
                 Debug.Log(hit.transform.name);
-                hit.transform.GetComponent<Enemy>().hit(statistics.damage);
+                hit.transform.GetComponent<Enemy>().hit(damage);
                 Debug.Log("HIT");
+                timerToEnd = 20.0f;
+            }
+            else
+            {
+                timerToEnd -= Time.deltaTime;
             }
             timeSinceLastShoot = 0;
         } else {
             timeSinceLastShoot += Time.deltaTime;
+        }
+
+        if(timerToEnd <=0)
+        {
+            SceneManager.LoadScene("FActory");
         }
     }
 

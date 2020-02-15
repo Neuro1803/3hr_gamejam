5897e5a3a8f4727fe7bd2c51560f36a36944cfd6
Paweł Pawlik <pawel.pawlik@equiqo.com>
2020-02-15 13:51:02 +0100

    tower and enemy init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public int speed;
+
+    public bool canAttack = false;
+
+    public void Update() {
+        if (canAttack) {
+
+        } else {
+            float translation = -1 * speed * Time.deltaTime;
+            transform.Translate(0, 0, translation, Space.World);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        canAttack = true;
+    }
+
+    private void OnCollisionExit(Collision other) {
+        canAttack = false;
+    }
+}
---
+++ Assets/Scripts/TowerStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New tower stats", menuName = "Tower Statistics")]
+public class TowerStatistics : ScriptableObject
+{
+    public float damage;
+    public float range;
+    public float fireRate;
+    public int multiTarget;
+
+    public float slow;
+    public float areaEffectRange;
+    public float areaEffectDamage;
+    public float damageOverTime;
+    public float knockback;
+}
